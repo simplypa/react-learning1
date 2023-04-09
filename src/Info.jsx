70a94405cb162fd6261692e8b78a2f31d2bdf3a8
YIFAN ZHANG <1012233721@qq.com>
2023-04-09 21:39:09 +1000

    feat: add nav and info component

---
+++ src/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      <h1>这是INFO的组件</h1>
+    </div>
+  )
+}
+
+export default Info
---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+            <a href="/">首页</a>
+        </li>
+        <li>
+            <a href="/info">信息</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
