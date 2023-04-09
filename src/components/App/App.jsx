3f67607e6f9b81ceb06dc8fa3519bc6997562c88
YIFAN ZHANG <1012233721@qq.com>
2023-04-10 00:12:46 +1000

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Nav from './Nav';
-import Info from './Info';
+import Nav from '../../Nav';
+import Info from '../../Info';
 
 function App() {
   return (
     <div className="App">
-      <h1>这是app.js 里的一个内容</h1>
       <Nav />
       <Info />
     </div>
