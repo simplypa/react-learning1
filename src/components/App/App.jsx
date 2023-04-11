a4a86c4d384d503068a6b0129973f83ff6df48b6
YIFAN ZHANG <1012233721@qq.com>
2023-04-11 22:25:39 +1000

    feat: add a show display function , use the hooks usestate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from 'components/Nav';
-import Button from 'components/Button';
+import DisplayInput from 'applications/DisplayInput';
 import 'styles/global.css';
 
 const App = () => (
   <div className="App">
     <h1 className="info">这是在app就是根component的位置制作的一个标题</h1>
-    <Button Name="测试按钮" />
+    <DisplayInput />
     <Nav />
   </div>
 );
