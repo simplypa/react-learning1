964f4f1ae5c3e0aa6ad6fec13f97714d7ae0e320
YIFAN ZHANG <1012233721@qq.com>
2023-04-11 20:13:52 +1000

    fix: change function to arrow function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,12 @@ import Nav from 'components/Nav';
 import Button from 'components/Button';
 import 'styles/global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="info">这是在app就是根component的位置制作的一个标题</h1>
-      <Button Name="测试按钮" />
-      <Nav />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1 className="info">这是在app就是根component的位置制作的一个标题</h1>
+    <Button Name="测试按钮" />
+    <Nav />
+  </div>
+);
 
 export default App;
