c1eeb9392ea0481e29d5c17147f02391d04975a4
YIFAN ZHANG <1012233721@qq.com>
2023-04-11 19:59:49 +1000

    fix: fix button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Button({ Name }) {
+  const [name, setName] = useState(Name);
+
   const buttonHandler = (msg) => {
     alert(`${msg} Button Clicked`);
+    setName('名字已经改变了');
   };
 
   return (
-    <button onClick={() => (buttonHandler({ Name }))} type="button">{Name}</button>
-  );
+    <button onClick={() => (buttonHandler({ Name }))} type="button">{name}</button>
+  );b 
 }
 
 Button.propTypes = {
