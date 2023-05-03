0d6015f6d9d9967c6fde5e74dcd980ccab6734ea
YIFAN ZHANG <1012233721@qq.com>
2023-05-03 23:18:01 +1000

    fix: modify server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+const dotent = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
+dotent.config();
+// npx nodemon server.js以启动服务器
 // 这里主要是云端不是mongodb，而是mongodb atlas，所以需要用到mongodb+srv://
-const uri = 'mongodb+srv://mitchellzhang0527:<QhTgIvXlFQftEKXF>@mitchdatabase.hlbvmps.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://mitchellzhang0527:<Tht950718>@mitchdatabase.hlbvmps.mongodb.net/?retryWrites=true&w=majority';
 
 async function connect() {
   await mongoose.connect(uri, {
     // authSouce: 'admin',
     user: 'mitchellzhang0527',
-    pass: 'QhTgIvXlFQftEKXF',
+    pass: 'Tht950718',
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(
@@ -21,11 +23,10 @@ async function connect() {
     });
 }
 
+connect();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-connect();
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
